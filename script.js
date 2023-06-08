7fe79db447c3ce74138be79bc908bf93781f54ba
Huesolo <Huesolo@users.noreply.github.com>
2023-06-08 20:25:17 -0300

    update

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function() {
-           document.querySelector(".nav-links").
-                      classList.toggle("nav-links-responsive")})                      
+  .addEventListener("click", function() {
+    document.querySelector(".nav-links")
+      .classList.toggle("nav-links-responsive");
+  });
+
 const form = document.querySelector('form');
-const nameInput = document.getElementById('name'); 
+const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
-form.addEventListener('submit', function(event) { event.preventDefault(); const name = nameInput.value;
-    const email = emailInput.value;
-    const message = messageInput.value;
-    console.log('Nombre:', name);
-    console.log('Email:', email);
-    console.log('Mensaje:', message);
-    nameInput.value = '';
-    emailInput.value = '';
-    messageInput.value = '';
-    alert('¡Formulario enviado con éxito!');
-  });
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const message = messageInput.value;
+
+  console.log('Nombre:', name);
+  console.log('Email:', email);
+  console.log('Mensaje:', message);
+
+  nameInput.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
+
+  alert('¡Formulario enviado con éxito!');
+});
